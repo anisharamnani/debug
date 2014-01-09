b0d684bcfceb7a0ae8bf60d60c155b95a7ca9e64
anisharamnani <anisha.ramnani@gmail.com>
2014-01-09 09:54:11 -0500

    debugged todo31

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,20 +4,34 @@ class GamesController < ApplicationController
     @games = Game.all
   end
 
-  def new
-    @game = Game.new
-  end
-
   def show
     @game = Game.find(params[:id])
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def edit 
+    @game = Game.find(params[:id])
+  end 
+
   # POST /games
   # POST /games.json
   def create
-    @game = Game.new(params[:games])
-    if @game.save
-      redirect_to @game
-    end
+    @game = Game.new(params[:game])
+    @game.save
+    redirect_to @game
   end
+
+  def update 
+    @game = Game.find(params[:id])
+  end 
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy 
+    redirect_to @game 
+  end 
+
 end
